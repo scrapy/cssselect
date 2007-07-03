60d55b0e0c2d64150dca961f1da7ebbe053dd776
Ian Bicking <ianb@colorstudy.com>
2007-07-03 00:10:56 +0000

    Add some fast translation for id, class, and plain element name matches
    git-svn-id: http://codespeak.net/svn/lxml/branch/html@44684 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

--- src/lxml/html/css.py
+++ src/lxml/html/css.py
@@ -474,8 +474,23 @@ class CombinedSelector(object):
 ##############################
 ## XPathExpr objects:
 
+_el_re = re.compile(r'^\w+\s*$')
+_id_re = re.compile(r'^(\w*)#(\w+)\s*$')
+_class_re = re.compile(r'^(\w*)\.(\w+)\s*$')
+
 def css_to_xpath(css_expr, prefix='descendant-or-self::'):
     if isinstance(css_expr, basestring):
+        match = _el_re.search(css_expr)
+        if match is not None:
+            return '%s%s' % (prefix, match.group(0).strip())
+        match = _id_re.search(css_expr)
+        if match is not None:
+            return "%s%s[@id = '%s']" % (
+                prefix, match.group(1) or '*', match.group(2))
+        match = _class_re.search(css_expr)
+        if match is not None:
+            return "%s%s[contains(concat(' ', normalize-space(@class), ' '), ' %s ')]" % (
+                prefix, match.group(1) or '*', match.group(2))
         css_expr = parse(css_expr)
     expr = css_expr.xpath()
     assert expr is not None, (
