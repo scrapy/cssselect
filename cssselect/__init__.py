518e3e1babcc3db38ab8afe948c05a4799693108
Adrián Chaves <adrian@chaves.io>
2019-08-09 11:17:29 +0200

    Bump version: 1.0.3 → 1.1.0

--- cssselect/__init__.py
+++ cssselect/__init__.py
@@ -18,5 +18,5 @@ from cssselect.parser import (parse, Selector, FunctionalPseudoElement,
 from cssselect.xpath import GenericTranslator, HTMLTranslator, ExpressionError
 
 
-VERSION = '1.0.3'
+VERSION = '1.1.0'
 __version__ = VERSION
