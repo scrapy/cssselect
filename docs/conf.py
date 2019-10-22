dde3b5e68ba2e49ec4552a75a805536c7dcdc896
Adrián Chaves <adrian@chaves.io>
2019-10-22 16:55:17 +0200

    Enable nitpicky Sphinx warnings, fix issues and fail on new issues

--- docs/conf.py
+++ docs/conf.py
@@ -248,3 +248,8 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
+
+
+# --- Nitpicking options ------------------------------------------------------
+
+nitpicky = True
