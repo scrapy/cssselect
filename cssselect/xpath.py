60c61469b8cdb5010ad0f81ff24a1329653dde69
Andrey Rahmatullin <wrar@wrar.name>
2022-10-27 16:13:55 +0500

    Restore and deprecate _unicode_safe_getattr (#133)

--- cssselect/xpath.py
+++ cssselect/xpath.py
@@ -14,6 +14,7 @@
 
 import re
 import typing
+import warnings
 from typing import Optional
 
 from cssselect.parser import (
@@ -35,6 +36,17 @@ from cssselect.parser import (
     SpecificityAdjustment,
     CombinedSelector,
 )
+
+
+@typing.no_type_check
+def _unicode_safe_getattr(obj, name, default=None):
+    warnings.warn(
+        "_unicode_safe_getattr is deprecated and will be removed in the"
+        " next release, use getattr() instead",
+        DeprecationWarning,
+        stacklevel=2,
+    )
+    return getattr(obj, name, default)
 
 
 class ExpressionError(SelectorError, RuntimeError):
